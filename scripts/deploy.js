096ce15a667a4fcdc230fc2aabc9040dfbcdae5d
Eren Yusuf Duran <erenyusufduran@gmail.com>
2022-08-07 02:29:05 +0300

    contract verification in etherscan

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,4 @@
-const { ethers } = require("hardhat");
+const { ethers, run, network } = require("hardhat");
 
 async function main() {
   const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
@@ -6,6 +6,26 @@ async function main() {
   const simpleStorage = await SimpleStorageFactory.deploy();
   await simpleStorage.deployed();
   console.log(`Deployed contract to: ${simpleStorage.address}`);
+  if (network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
+    await simpleStorage.deployTransaction.wait(6);
+    await verify(simpleStorage.address, []);
+  }
+}
+
+async function verify(contractAddress, args) {
+  console.log("Verifying contract...");
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  } catch (e) {
+    if (e.message.toLowerCase().includes("already verified")) {
+      console.log("Already Verified!");
+    } else {
+      console.log(e);
+    }
+  }
 }
 
 main()
